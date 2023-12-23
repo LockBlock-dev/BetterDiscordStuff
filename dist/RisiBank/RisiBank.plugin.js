5911998c5a85c7b408b4259e1445fab950f5a1f9
LockBlock-dev <68129141+LockBlock-dev@users.noreply.github.com>
2023-12-23 20:33:13 +0100

    correct version number

--- dist/RisiBank/RisiBank.plugin.js
+++ dist/RisiBank/RisiBank.plugin.js
@@ -2,7 +2,7 @@
  * @name RisiBank
  * @author LockBlock
  * @description Brings RisiBank to the Discord client.
- * @version 4.1.0
+ * @version 4.1.1
  * @donate https://ko-fi.com/lockblock
  * @source https://github.com/LockBlock-dev/BetterDiscordStuff/tree/master/risibank
  */
